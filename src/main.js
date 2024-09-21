ca9e379b7f0b8ac4fdbad6f63a9af34e720b1117
micheleboanno92 <michelebonanno92@gmail.com>
2024-09-21 22:12:23 +0200

    importato icons bootstrap

--- src/main.js
+++ src/main.js
@@ -3,5 +3,7 @@ import App from './App.vue'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+import "bootstrap-icons/font/bootstrap-icons.css"
+
 
 createApp(App).mount('#app')
