e6b3cad660a9d2c540fbf056758b9688a260f42b
micheleboanno92 <michelebonanno92@gmail.com>
2024-09-24 10:37:07 +0200

    creata struttura card

--- src/components/MainSectionFive.vue
+++ src/components/MainSectionFive.vue
@@ -27,12 +27,21 @@ export default {
         {{ titleSection }}
       </p>
       <ul class="d-flex align-items-center justify-content-center">
-          <li v-for="(link, i) in listLink" :key="i" class="me-5 mt-3" >
+          <li v-for="(link, i) in listLink" :key="i" class="me-4 mt-3" >
             <a href="#" @click.prevent>
               {{ link }} 
             </a>
           </li>
         </ul>
+        <div class="my-row">
+            <div class="my-card bg-warning" v-for="(card, i) in 12" :key="i" >
+                CARD
+            </div>
+           
+          
+        </div>
+   
+     
   </div>
 </template>
 
@@ -45,6 +54,17 @@ export default {
     list-style-type: none;
   }a{
     text-decoration: none;
+    font-size: 0.9rem;
+  }.my-row{
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .my-card{
+    width: calc((100% / 6) - 20px );
+    height: 300px;
+    margin-bottom: 30px;
+    margin-left: 10px;
+    margin-right: 10px;
   }
 
 </style>
